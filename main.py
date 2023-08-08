8cf2e30a770d0b1e3e5fbd02e20e534fe47039ef
chelscelis <celis.chelseatrisha@gmail.com>
2023-08-08 16:45:53 +0800

    main test split removed

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import warnings
 import streamlit as st 
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 nltk.download('stopwords')
