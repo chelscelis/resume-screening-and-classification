4f55614ac498cf98c9255c8ec81bb446f9687dce
chelscelis <celis.chelseatrisha@gmail.com>
2023-09-05 22:39:14 +0800

    filter feature bug fix

--- utils.py
+++ utils.py
@@ -69,13 +69,13 @@ def dimensionalityReduction(features):
     return features    
 
 def filterDataframeClf(df: pd.DataFrame) -> pd.DataFrame:
-    modify = st.toggle("Add filters", key = 'toggle-Clf')
+    modify = st.toggle("Add filters", key = 'toggle-clf-1')
     if not modify:
         return df
     df = df.copy()
     modificationContainer = st.container()
     with modificationContainer:
-        toFilterColumns = st.multiselect("Filter table on", df.columns)
+        toFilterColumns = st.multiselect("Filter table on", df.columns, key = 'toggle-clf-2')
         for column in toFilterColumns:
             left, right = st.columns((1, 20))
             left.write("↳")
@@ -84,28 +84,10 @@ def filterDataframeClf(df: pd.DataFrame) -> pd.DataFrame:
                     f'Values for {column}',
                     df[column].unique(),
                     default = list(df[column].unique()),
+                    key = 'toggle-clf-3'
                 )
                 df = df[df[column].isin(userCatInput)]
-            else:
-                userTextInput = right.text_input(
-                    f'Substring or regex in {column}',
-                )
-                if userTextInput:
-                    df = df[df[column].astype(str).str.contains(userTextInput)]
-    return df
-
-def filterDataframeRnk(df: pd.DataFrame) -> pd.DataFrame:
-    modify = st.toggle("Add filters", key = 'toggle-rnk')
-    if not modify:
-        return df
-    df = df.copy()
-    modificationContainer = st.container()
-    with modificationContainer:
-        toFilterColumns = st.multiselect("Filter table on", df.columns)
-        for column in toFilterColumns:
-            left, right = st.columns((1, 20))
-            left.write("↳")
-            if is_numeric_dtype(df[column]):
+            elif is_numeric_dtype(df[column]):
                 _min = float(df[column].min())
                 _max = float(df[column].max())
                 step = (_max - _min) / 100
@@ -115,11 +97,54 @@ def filterDataframeRnk(df: pd.DataFrame) -> pd.DataFrame:
                     max_value = _max,
                     value = (_min, _max),
                     step = step,
+                    key = 'toggle-clf-4'
                 )
                 df = df[df[column].between(*userNumInput)]
             else:
                 userTextInput = right.text_input(
                     f'Substring or regex in {column}',
+                    key = 'toggle-clf-5'
+                )
+                if userTextInput:
+                    df = df[df[column].astype(str).str.contains(userTextInput)]
+    return df
+
+def filterDataframeRnk(df: pd.DataFrame) -> pd.DataFrame:
+    modify = st.toggle("Add filters", key = 'toggle-rnk-1')
+    if not modify:
+        return df
+    df = df.copy()
+    modificationContainer = st.container()
+    with modificationContainer:
+        toFilterColumns = st.multiselect("Filter table on", df.columns, key = 'toggle-rnk-2')
+        for column in toFilterColumns:
+            left, right = st.columns((1, 20))
+            left.write("↳")
+            if is_categorical_dtype(df[column]):
+                userCatInput = right.multiselect(
+                    f'Values for {column}',
+                    df[column].unique(),
+                    default = list(df[column].unique()),
+                    key = 'toggle-rnk-3'
+                )
+                df = df[df[column].isin(userCatInput)]
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                userNumInput = right.slider(
+                    f'Values for {column}',
+                    min_value = _min,
+                    max_value = _max,
+                    value = (_min, _max),
+                    step = step,
+                    key = 'toggle-rnk-4'
+                )
+                df = df[df[column].between(*userNumInput)]
+            else:
+                userTextInput = right.text_input(
+                    f'Substring or regex in {column}',
+                    key = 'toggle-rnk-5'
                 )
                 if userTextInput:
                     df = df[df[column].astype(str).str.contains(userTextInput)]
@@ -183,4 +208,136 @@ def resumesRank(jobDescriptionRnk, resumeRnk):
     resumeRnk = resumeRnk.sort_values(by='Similarity Score (%)', ascending=False)
     del resumeRnk['cleanedResume']
     return resumeRnk
+
+def writeGettingStarted():
+    st.write("""
+    ## Hello, Welcome!  
+    In today's competitive job market, the process of manually screening resumes has become a daunting task for recruiters and hiring managers. 
+    The sheer volume of applications received for a single job posting can make it extremely time-consuming to identify the most suitable candidates efficiently. 
+    This often leads to missed opportunities and the potential loss of top-tier talent.
+
+    The ***Resume Screening & Classification*** website application aims to help alleviate the challenges posed by manual resume screening. The objectives are:
+    - To classify the resumes into their most suitable job industry category
+    - To compare the resumes to the job description and rank them by similarity
+    """)
+    st.divider()
+    st.write("""
+    ## Input Guide 
+    #### For the Job Description: 
+    Ensure the job description is saved in a text (.txt) file. 
+    Clearly outline the responsibilities, qualifications, and skills associated with the position.
+
+    #### For the Resumes: 
+    Resumes must be compiled in an excel (.xlsx) file. 
+    The organization of columns is up to you but ensure that the "Resume" column is present.
+    The values under this column should include all the relevant details of the resume.
+    You may refer to the sample format below.
+    """)
+    with st.expander('View sample format'):
+        st.write('# Hello world')
+    st.write("""
+    #### Demo Files: 
+    We have included input files for demo testing purposes. 
+    Fifty-five resumes (five resumes per category) were collected from LinkedIn with personally identifiable information (PII) removed.
+    Then, eleven job descriptions were collected from Indeed and Glassdoor.
+    You can download the following files to experience the capabilities of the web app:
+
+    - **Access Job Description files [here]()**
+    - **Access Resume files [here]()**
+    """)
+    st.divider()
+    st.write("""
+    ## Demo Walkthrough
+    #### Classify Tab:
+    The web app will classify the resumes into their most suitable job industry category.
+    Currently the Category Scope consists of the following:
+    """)
+    column1, column2 = st.columns(2)
+    with column1:
+        st.write("""
+        - Aviation
+        - Business development
+        - Culinary
+        - Education
+        - Engineering
+        - Finance
+        """)
+    with column2:
+        st.write("""
+        - Fitness
+        - Healthcare
+        - HR
+        - Information Technology
+        - Public relations
+        """)
+    with st.expander('Classification Steps'):
+        st.write("""
+        ##### Upload Resumes:
+        - Navigate to the "Classify" tab.
+        - Click the "Upload Resumes" button.
+        - Select the Excel file (.xlsx) containing the resumes you want to classify. Ensure that your Excel file has the "Resume" column with the resume text and any necessary columns for filtering or additional information.
+        ######
+        """)
+        st.write("""
+        ##### Start Processing:
+        - Click the "Start Processing" button.
+        - The app will analyze the resumes and categorize them into job industry categories.
+        ######
+        """)
+        st.write("""
+        ##### View Classification Results:
+        - A bar chart will appear, showing the number of resumes per category, helping you visualize the distribution.
+        - Below the bar chart, there is a dataframe that displays the list of resumes along with their assigned categories.
+        ######
+        """)
+        st.write("""
+        ##### Add Filters:
+        - You can apply filters to the dataframe to narrow down your results.
+        ######
+        """)
+        st.write("""
+        ##### Donwload Results:
+        - Once you've applied filters or are satisfied with the results, you can download the current dataframe as an Excel file by clicking the "Save Current Output as XLSX" button.
+        ####
+        """)
+    st.write("""
+    #### Rank Tab:
+    The web app will rank the resumes based on their similarity to the job description. 
+    Keep in mind, the ranking placement does not imply that one applicant is absolutely better than the other.
+    Interviews and other tests are still preferred before making final decisions.
+
+    """)
+    with st.expander('Ranking Steps'):
+        st.write("""
+        ##### Upload Job Description and Resumes:
+        - Navigate to the "Rank" tab.
+        - Upload the job description as a text file. This file should contain the description of the job you want to compare resumes against.
+        - Upload the Excel file that contains the resumes you want to rank.
+        ######
+        """)
+        st.write("""
+        ##### Start Processing:
+        - Click the "Start Processing" button.
+        - The app will analyze the job description and rank the resumes based on their similarity to the job description.
+        ######
+        """)
+        st.write("""
+        ##### View Classification Results:
+        - The output will display the contents of the job description.
+        - Below the job description, there is a dataframe that lists the ranked resumes along with their similarity scores.
+        ######
+        """)
+        st.write("""
+        ##### Add Filters:
+        - You can apply filters to the dataframe to narrow down your results.
+        ######
+        """)
+        st.write("""
+        ##### Donwload Results:
+        - Once you've applied filters or are satisfied with the results, you can download the current dataframe as an Excel file by clicking the "Save Current Output as XLSX" button.
+        ####
+        """)
+    # TODO: add sample resume dataframe format
+    # TODO: add picture per step
+    # TODO: add file links
 
